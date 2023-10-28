f8b718450cad78828477e4901ded9578f5fce65a
Prashant047 <prash.kumar047@gmail.com>
2023-10-28 16:52:00 +0100

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,97 @@
-import Image from 'next/image'
+'use client';
+import { Sprout, Pencil, Coffee, Car } from "lucide-react"
+import { motion, LayoutGroup, AnimatePresence  } from 'framer-motion';
+import { useState } from 'react';
+
+const widgetIconMap = {
+  'sprout': <Sprout/>,
+  'car': <Car/>,
+  'coffee': <Coffee/>,
+  'pencil': <Pencil/>
+};
+
+const drawrVariant = {
+  close: {
+  },
+  open: {
+  }
+};
+
+const buttonContVariant = {
+  close: {
+    y: '120%'
+  },
+  open: {
+    y: 0
+  }
+};
 
 export default function Home() {
+
+  const [active, setActive] = useState<string| null>(null);
+  const nothingSelected = active === null;
+  const setToNull = () => setActive(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main>
+      <div className="mx-auto mt-[150px] mb-7 bg-neutral-800 w-[200px] h-[200px] rounded-[25px] text-neutral-200 overflow-hidden">
+        <LayoutGroup>
+          <AnimatePresence mode="popLayout">
+            {nothingSelected?(
+              <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="h-full grid grid-cols-2 grid-rows-2 place-items-center">
+                <motion.div whileHover={{scale: 1.05}} whileTap={{scale:0.9}} key="sprout" onClick={() => setActive('sprout')} layoutId="sprout" className="h-16 w-16 border-2 flex items-center justify-center rounded-full cursor-pointer">
+                  <Sprout/>
+                </motion.div>
+                <motion.div  whileHover={{scale: 1.05}} whileTap={{scale:0.9}} key="pencil"  onClick={() => setActive('pencil')}layoutId="pencil" className="h-16 w-16 border-2 flex items-center justify-center rounded-full cursor-pointer">
+                  <Pencil/>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.05}} whileTap={{scale:0.9}} key="coffee"  onClick={() => setActive('coffee')} layoutId="coffee" className="h-16 w-16 border-2 flex items-center justify-center rounded-full cursor-pointer">
+                  <Coffee/>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.05}} whileTap={{scale:0.9}} key="car"  onClick={() => setActive('car')} layoutId="car" className="h-16 w-16 border-2 flex items-center justify-center rounded-full cursor-pointer">
+                  <Car/>
+                </motion.div>
+              </motion.div>
+            ):(
+              <motion.div 
+                className="h-full p-4 rounded-[25px]"
+                variants={drawrVariant}
+                initial="close"
+                animate="open"
+                exit="close"
+                key={`drawr`}
+              >
+                <motion.div 
+                  key={active} 
+                  layoutId={active}  
+                  transition={{ease: [0.32, 0.72, 0, 1]}}
+                  className="h-16 w-16 mb-4 mx-auto border-2 flex items-center justify-center rounded-full"
+                >
+                  {/* @ts-ignore */}
+                  {widgetIconMap[active]}
+                </motion.div>
+                <div className="overflow-hidden">
+                  <motion.div 
+                    className="flex flex-col text-neutral-800 gap-2"
+                    variants={buttonContVariant}
+                    transition={{ ease:[0.32, 0.72, 0, 1]}}
+                  >
+                    <button onClick={setToNull} className="bg-green-500 py-2 rounded-lg font-bold hover:bg-green-500/90 transition">complete</button>
+                    <button onClick={setToNull} className="bg-neutral-200 py-2 rounded-lg font-bold hover:bg-neutral-200/90 transition">open app</button>
+                  </motion.div>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </LayoutGroup>
+      </div>
+      <div className="px-4">
+        <p className="text-center">
+          widget micro-interaction with <b>framer-motion</b> layout animations
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p className="text-center text-neutral-500">
+          click or hover over the icons
+        </p>
       </div>
     </main>
   )
